6e012268e92f624ee6d64f911cfbcc91e3664650
Patrick Niepel <patrick@pace.car>
2021-05-25 09:47:26 +0200

    Add custom url protocol if needed

--- Templates/Swift/Common/APIClient+URLProtocol.swift
+++ Templates/Swift/Common/APIClient+URLProtocol.swift
@@ -4,7 +4,9 @@ import Foundation
 
 public extension {{ options.name }}Client {
     private static var urlConfiguration: URLSessionConfiguration {
-        .default
+        let config = URLSessionConfiguration.default
+        config.setCustomURLProtocolIfAvailable()
+        return config
     }
 
     static var custom = {{ options.name }}Client(baseURL: {{ options.name }}Client.default.baseURL, configuration: urlConfiguration)
